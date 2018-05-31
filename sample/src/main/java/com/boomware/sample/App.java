50f6f563fd399415a88c4d10d8437e52e0ee250d
Stanislav Borisychev <s.borisychev@gmail.com>
2018-05-31 19:52:15 +0300

    initial

--- sample/src/main/java/com/boomware/sample/App.java
+++ sample/src/main/java/com/boomware/sample/App.java
@@ -7,12 +7,11 @@ import com.boomware.sdk.Boomware;
 
 public class App extends Application {
 
-    public static final String API_KEY = "97b0763d2c6220225bb997cad7b07d79:40c8d8d42beb9c307c56d1e817a9ba56:4bfdf6e7f72fe5b3";
 
     public void onCreate(){
         super.onCreate();
 
         //Initialize Boomware instance (key from Account on https://boomware.com)
-        Boomware.initialize(this, API_KEY);
+        Boomware.initialize(this, "<your:api:jey>");
     }
 }
